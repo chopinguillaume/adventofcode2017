808e717dc83fbbe9a54b22fd6b083acd41cde8ec
Guillaume <guillaumechopin35@gmail.com>
2017-12-28 23:58:37 +0100

    Day03 Part2

--- src/main/java/day03/ManhattanDistance.java
+++ src/main/java/day03/ManhattanDistance.java
@@ -40,4 +40,77 @@ public class ManhattanDistance {
 
         return distance;
     }
+
+
+    static int partie2(int nombre) {
+
+        int taille = 15;
+        int[][] grille = new int[taille][taille];
+
+        int x = taille / 2;
+        int y = taille / 2;
+
+        grille[x][y] = 1;
+
+        int cercle = 0;
+
+        int dx = 0;
+        int dy = 0;
+
+        int dir = 0; //0 : droite, 1 : haut, 2 : gauche, 3 : bas
+
+        int valeur = 1;
+
+        while (valeur <= nombre) {
+            switch (dir) {
+                case 0:
+                    if (dx < cercle) dx++;
+                    if (dx == cercle && dy > -cercle) dir = 1;
+                    break;
+                case 1:
+                    if (dy < cercle) dy++;
+                    if (dy == cercle) dir = 2;
+                    break;
+                case 2:
+                    if (dx > -cercle) dx--;
+                    if (dx == -cercle) dir = 3;
+                    break;
+                case 3:
+                    if (dy > -cercle) dy--;
+                    if (dy == -cercle) dir = 0;
+                    break;
+            }
+
+            //Fin du cercle, en bas a droite
+            if (dx == cercle && dy == -cercle) {
+                cercle++;
+            }
+
+            valeur = valeur(grille, x + dx, y + dy);
+            grille[x + dx][y + dy] = valeur;
+        }
+
+        return valeur;
+    }
+
+
+    private static int valeur(int[][] grille, int x1, int y1) {
+
+        int somme = 0;
+
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                somme += grille[x1 + dx][y1 + dy];
+            }
+        }
+
+        return somme;
+    }
+
+
+    public static void main(String[] args) {
+
+        partie2(60);
+    }
+
 }
--- src/test/java/day03/Test03.java
+++ src/test/java/day03/Test03.java
@@ -17,6 +17,7 @@ public class Test03 {
         assertEquals(3, ManhattanDistance.distance(12));
         assertEquals(4, ManhattanDistance.distance(17));
         assertEquals(6, ManhattanDistance.distance(37));
+        assertEquals(6, ManhattanDistance.distance(127));
         assertEquals(31, ManhattanDistance.distance(1024));
 
         System.out.println(ManhattanDistance.distance(277678));
@@ -25,6 +26,14 @@ public class Test03 {
 
     @Test
     public void test02() {
+
+        assertEquals(2, ManhattanDistance.partie2(1));
+        assertEquals(11, ManhattanDistance.partie2(10));
+        assertEquals(54, ManhattanDistance.partie2(27));
+        assertEquals(122, ManhattanDistance.partie2(60));
+        assertEquals(806, ManhattanDistance.partie2(800));
+
+        System.out.println(ManhattanDistance.partie2(277678));
 
     }
 
